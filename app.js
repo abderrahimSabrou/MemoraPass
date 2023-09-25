628605f80ffc86c9bab2ae8313a1db56d4ffd064
abderrahimSabrou <abderrahimsabrou@gmail.com>
2023-09-25 17:52:55 +0100

    add the ability to generate multiple password at once

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const passowrdBox = document.querySelector("#password");
 const passowrdBox1 = document.querySelector("#password1");
+const passowrdBox2 = document.querySelector("#password2");
+const passowrdBox3 = document.querySelector("#password3");
+const passowrdBox4 = document.querySelector("#password4");
+const passowrdBox5 = document.querySelector("#password5");
+const passowrdBox6 = document.querySelector("#password6");
+const passowrdBox7 = document.querySelector("#password7");
+const passowrdBox8 = document.querySelector("#password8");
+const passowrdBox9 = document.querySelector("#password9");
+const passowrdBox10 = document.querySelector("#password10");
+const passowrdBox11 = document.querySelector("#password11");
+const passowrdBox12 = document.querySelector("#password12");
+const passowrdBox13 = document.querySelector("#password13");
+const passowrdBox14 = document.querySelector("#password14");
+
 const length = 8;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -34,6 +48,188 @@ function createPassword1() {
   passowrdBox1.value = password;
 }
 
+function createPassword2() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox2.value = password;
+}
+
+function createPassword3() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox3.value = password;
+}
+
+function createPassword4() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox4.value = password;
+}
+
+function createPassword5() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox5.value = password;
+}
+
+function createPassword6() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox6.value = password;
+}
+
+function createPassword7() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox7.value = password;
+}
+
+function createPassword8() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox8.value = password;
+}
+
+function createPassword9() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox9.value = password;
+}
+
+function createPassword10() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox10.value = password;
+}
+
+function createPassword11() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox11.value = password;
+}
+
+function createPassword12() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox12.value = password;
+}
+
+function createPassword13() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox13.value = password;
+}
+
+function createPassword14() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  passowrdBox14.value = password;
+}
+
 const generatebtn = document.getElementById("generate");
 generatebtn.addEventListener("click", createPassword);
 generatebtn.addEventListener("click", createPassword1);
+generatebtn.addEventListener("click", createPassword2);
+generatebtn.addEventListener("click", createPassword3);
+generatebtn.addEventListener("click", createPassword4);
+generatebtn.addEventListener("click", createPassword5);
+generatebtn.addEventListener("click", createPassword6);
+generatebtn.addEventListener("click", createPassword7);
+generatebtn.addEventListener("click", createPassword8);
+generatebtn.addEventListener("click", createPassword9);
+generatebtn.addEventListener("click", createPassword10);
+generatebtn.addEventListener("click", createPassword11);
+generatebtn.addEventListener("click", createPassword12);
+generatebtn.addEventListener("click", createPassword13);
+generatebtn.addEventListener("click", createPassword14);
